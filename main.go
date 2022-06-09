c8b7ec993c0da52db2c0933a176f928132b6a676
KJeon10 <102023899+KJeon10@users.noreply.github.com>
2022-06-09 15:49:09 +0900

    saving before rework
    Current Version doesn't work well with SegWit Transaction.
    Reworking with new structures

--- main.go
+++ main.go
@@ -1,126 +1,111 @@
 package main
 
 import (
+	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"io/ioutil"
 	"log"
 )
 
 func main(){
-	type PreAmble struct {
-		magicNumber []byte
-		blockSize []byte
-	}
-	type BlockHeader struct {
-		blockVersion []byte
-		prevBlockHash []byte
-		merkleRoot []byte
-		time []byte
-		bits []byte
-		nonce []byte
-		totalTxNum []byte
-		version []byte
-		numInput []byte
-		numOutput []byte 
-		lockTime []byte
-	}
-
-	 type TxData struct{
-		 numTx uint32
-	 }
-
-	type TxInput struct {
-		prevTxhash []byte
-		prevTxoutIndex []byte
-		scriptLength []byte
-		script []byte
-		sequence []byte
-	}
-
-	type TxOutput struct {
-		outputValue []byte
-		scriptLength []byte
-		script []byte
-	}
-	type Block struct {
-		PreAmble PreAmble
-		BlockHeader BlockHeader
-		TxData TxData
-	}
-
-	blockFile, err := ioutil.ReadFile("/Users/jeonkangmin/Desktop/blk00000.dat") // Upload whole block file to buffer
+	blockFile, err := ioutil.ReadFile("/Users/jeonkangmin/Desktop/blk03063.dat") // Upload whole block file to buffer
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	blockData := blockFile [0:]
+
+	preAmble := blockData[0:8]
+
+	blockHeader := blockData[8:96]
+
+	numTx, txStartPos := VariableHandler(blockData[88:97], 88)
+
+	transactions := blockData[txStartPos:]
+
+	fmt.Printf("MagicNumber : %s\n", hex.EncodeToString(preAmble[0:4]))	//magicnumber
+	fmt.Printf("BlockSize : %s\n", hex.EncodeToString(preAmble[4:8])) //blocksize
+
+	fmt.Printf("Version : %s\n", hex.EncodeToString(blockHeader[0:4])) //version
+	fmt.Printf("PrevBlockHash : %s\n", hex.EncodeToString(blockHeader[4:36]))	//preblockhash
+	fmt.Printf("MerkleRoot :%s\n", hex.EncodeToString(blockHeader[36:68])) // merkleroot
+	fmt.Printf("Time : %s\n", hex.EncodeToString(blockHeader[68:72])) // time
+	fmt.Printf("Bits : %s\n", hex.EncodeToString(blockHeader[72:76])) // bits
+	fmt.Printf("Nonce : %s\n", hex.EncodeToString(blockHeader[76:80])) //nonce
+
+	fmt.Printf("numTx : %d\n", numTx) // number of Transactions 
+ 	for Index:=0; uint64(Index)<numTx; Index++{
+	transactions = TxParser(transactions, uint64(Index))
+ }
 
 	
-	//x:=0
-	fmt.Printf("%X\n\n\n\n\n", blockFile[89])
-	
-	var P uint32 = 0
-	/*fmt.Print(blockFile[88:97])
-	a, b := VariableHandler(blockFile[88:97], howBytes)
-	fmt.Printf("%d and %d", a, b)*/
-
-	magicNumber := blockFile[0:4]
-	blockSize := blockFile[4:8]
-	blockVersion:= blockFile[8:12]
-	prevBlockHash:= blockFile[12:44]
-	merkleRoot:= blockFile[44:76]
-	time:= blockFile[76:80]
-	bits:= blockFile[80:84]
-	nonce:= blockFile[84:88]
-	numTx, P := VariableHandler(blockFile[88:97], 88)
-	fmt.Println(P)
-	prevTxhash := blockFile[P:P+32]
-	prevTxoutIndex := blockFile[P+32:P+36]
-	inputScriptLength, P := VariableHandler((blockFile[P+36:P+40]), P+36)
-	fmt.Println(P)
-	var L uint32
-	L = 5
-	inputScript := blockFile[P:P+L]
-	fmt.Printf("magicNumber : %X\nblockSize : %X\nblockVersion : %X\nprevBlockHash : %X\nmerkleRoot : %X\ntime : %X\nbits : %X\nnonce : %X\nnumTx : %X\nprevTxHash : %X\npreTxOutIndex : %X\ninputScriptLength : %X\ninputScript : %X\n", magicNumber, blockSize, blockVersion, prevBlockHash, merkleRoot, time, bits, nonce, numTx, prevTxhash, prevTxoutIndex, inputScriptLength, inputScript)
-	
-
-/*	blockData := &Block{
-		PreAmble : PreAmble{
-			magicNumber: blockFile[0:4],
-			blockSize: blockFile[5:9],
-		},
-		BlockHeader : BlockHeader{
-			blockVersion: blockFile[8:12],
-			prevBlockHash: blockFile[12:44],
-			merkleRoot: blockFile[44:76],
-			time: blockFile[76:80],
-			bits: blockFile[80:84],
-			nonce: blockFile[84:88],
-		},
-		TxData : TxData {
-			numTx: ,
-		},
 	}
-	*/
 
-
-
-}
-
-	func VariableHandler(current []byte, newPointer uint32) ([]byte, uint32){
+	func VariableHandler(current []byte, currentPos uint64) (uint64, uint64){
 		switch {
-		case current[0] < 253:
-			newPointer += 1
-			return current[0:1], newPointer
+		case current[0] == 0:
+			return VariableHandler(current[2:], currentPos+2)
+		case current[0] < 253 && current[0] > 0:
+			return uint64(current[0]), currentPos+1
 		case current[0] == 253:
-			newPointer += 2
-			return current[1:2], newPointer
+			return uint64(binary.LittleEndian.Uint16(current[1:3])), currentPos+3
 		case current[0] == 254:
-			newPointer+= 5
-			return current[1:5], newPointer
+			return uint64(binary.LittleEndian.Uint32(current[1:5])), currentPos+5
 		case current[0] == 255:
-			newPointer+=8
-			return current[1:9], newPointer
+			return binary.LittleEndian.Uint64(current[1:9]), currentPos+9
 		default:
-			return current[0:1], newPointer
+			return 0, 0
 		}
+	}
+
+	func TxParser(TxData []byte, Index uint64)(NewTxData []byte){
+		fmt.Printf("===========START TRANSACTION INDEX %d===========\n\n\n\n\n", Index)
+		fmt.Printf("Version : %s\n", hex.EncodeToString(TxData[0:4]))
+
+		numInput, InputStartPos := VariableHandler(TxData[4:], 4)
+
+		fmt.Printf("Number of input : %d\n", numInput)
+
+		OutputPos := InputParser(TxData[InputStartPos:], numInput) + InputStartPos
+
+		numOutput, OutputStartPos := VariableHandler(TxData[OutputPos:], OutputPos)
+		fmt.Printf("Number of Output : %d\n", numOutput)
+
+		NewTxData = OutputParser(TxData[OutputStartPos:], numOutput)
+		fmt.Printf("\n\n\n\n\n===========END TRANSACTION INDEX %d===========\n\n\n\n\n", Index)
+		return NewTxData
+	}
+
+
+	func InputParser(InputData []byte, numInput uint64) (OutputStartPos uint64){
+		var Pos uint64 = 0
+		for Index:=0; uint64(Index)<numInput; Index++{
+		fmt.Printf("\n\n###INPUT INDEX %d###\n\n", Index)
+		fmt.Printf("Previous Tx Hash : %s\n", hex.EncodeToString(InputData[Pos:Pos+32]))
+		fmt.Printf("Previous TxOut Index : %s\n", hex.EncodeToString(InputData[Pos+32:Pos+36]))
+		ScriptLength, ScriptStartPos := VariableHandler(InputData[Pos+36:Pos+45], Pos+36)
+		fmt.Printf("Script Length : %d\n", ScriptLength)
+		fmt.Printf("Input Script : %s\n", hex.EncodeToString(InputData[ScriptStartPos:ScriptStartPos+ScriptLength]))
+		fmt.Printf("Sequence : %s\n", hex.EncodeToString(InputData[ScriptStartPos+ScriptLength:ScriptStartPos+ScriptLength+4]))
+		fmt.Printf("\n\n###END OF INPUT INDEX%d###\n\n", Index)
+		Pos = ScriptStartPos+ScriptLength+4
+		}
+		return Pos
+		
+	}
+
+	func OutputParser(OutputData []byte, numOutput uint64)(NewTx []byte){ 
+		var Pos uint64 = 0
+		for Index:=0; uint64(Index)<numOutput; Index++{
+		fmt.Printf("\n\n###OUTPUT INDEX %d###\n\n", Index)
+		fmt.Printf("OUTPUT VALUE : %d\n", binary.LittleEndian.Uint64(OutputData[Pos:Pos+8]))
+		ScriptLength, ScriptStartPos := VariableHandler(OutputData[Pos+8:], Pos+8)
+		fmt.Printf("Script Length : %d\n", ScriptLength)
+		fmt.Printf("Output Script : %s\n", hex.EncodeToString(OutputData[ScriptStartPos:ScriptStartPos+ScriptLength]))
+		Pos = ScriptStartPos+ScriptLength
+		}
+		fmt.Printf("LockTime: %d\n",binary.LittleEndian.Uint32(OutputData[Pos:Pos+4]))
+		fmt.Printf("\n\n### END OF OUTPUT ###\n\n")
+		return OutputData[Pos+2:]
 	}
