d03ee2e64385d3b468f40d1eebb74a1432ac21d7
KJeon10 <102023899+KJeon10@users.noreply.github.com>
2022-06-09 19:18:43 +0900

    update
    learning some new skills https://gosamples.dev/read-file/

--- Block.go
+++ Block.go
@@ -1,8 +1,20 @@
 package main
 
+type Block struct {
+	Preamble     Preamble
+	BlockHeader  BlockHeader
+	TotalTxCount TotalTxCount
+	Transactions []Transaction
+}
+
 type BlockInfo struct {
 	HeaderHash []byte // Version + Previous Block Hash + Merkle Root Hash + Time + Bits + nonce
 	// https://dlt-repo.net/how-to-calculate-a-bitcoin-block-hash-manually/ will use step 2~ from this article
+}
+
+type Preamble struct {
+	MagicNumber []byte
+	BlockSize   uint32
 }
 type BlockHeader struct {
 	BlockVersion  []byte
@@ -19,9 +31,28 @@ type TotalTxCount struct {
 
 type Transaction struct {
 	TxVersion     uint32
+	IsSegwit      bool
 	TxInputCount  uint64
 	TxInput       []TxInput
 	TxOutputCount uint64
 	TxOutput      []TxOutput
 	LockTime      uint32
 }
+
+type TxInput struct {
+	PrevTxHash     []byte
+	PrevTxOutIndex int32 // Signed Integer(4Bytes)
+	ScriptLength   uint64
+	Script         []byte // Length is specified by ScriptLength
+	Sequence       int32
+}
+
+type TxOutput struct {
+	OutputValue  uint64
+	ScriptLength uint64
+	Script       []byte
+}
+
+// TXID => nVersion + txins + txouts + nLocktime
+// https://github.com/bitcoin/bips/blob/master/bip-0144.mediawiki
+// Weight => Version*4, Marker*1, Flag*1, InputCount*1, Input*4, OutputCount*4, Output*4, Witness*1, Locktime*4
--- main.go
+++ main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
+
+}
+
+func OpenBlockfile() (BlockFile []byte) {
+	BlockFile, err := os.ReadFile("/Users/jeonkangmin/Desktop/blk03063.dat") // Upload whole block file to buffer
+	if err != nil {
+		log.Fatal(err)
+	}
+	return BlockFile
+}
+
+// Types used : []byte, uint32, uint64, bool, int32
+func RetrnNbytes() {
+
 }
