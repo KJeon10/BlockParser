869024937c3237b375fc98d373824d22293675c4
KJeon10 <102023899+KJeon10@users.noreply.github.com>
2022-06-09 16:15:35 +0900

    Update Block.go

--- Block.go
+++ Block.go
@@ -1,13 +1,27 @@
 package main
 
 type BlockInfo struct {
-	HeaderHash string // Version + Previous Block Hash + Merkle Root Hash + Time + Bits + nonce
+	HeaderHash []byte // Version + Previous Block Hash + Merkle Root Hash + Time + Bits + nonce
+	// https://dlt-repo.net/how-to-calculate-a-bitcoin-block-hash-manually/ will use step 2~ from this article
 }
 type BlockHeader struct {
-	BlockVersion  int32
-	PrevBlockHash string
-	MerKleRoot    string
+	BlockVersion  []byte
+	PrevBlockHash []byte
+	MerKleRoot    []byte
 	Time          uint32
 	Bits          uint32
 	Nonce         uint32
+}
+
+type TotalTxCount struct {
+	TotalTxCount uint64
+}
+
+type Transaction struct {
+	TxVersion     uint32
+	TxInputCount  uint64
+	TxInput       []TxInput
+	TxOutputCount uint64
+	TxOutput      []TxOutput
+	LockTime      uint32
 }
