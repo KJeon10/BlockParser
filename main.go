ed529f115ce281147122415a66af9f30a62f029d
KJeon10 <102023899+KJeon10@users.noreply.github.com>
2022-06-11 16:01:33 +0900

    pointers
    added

--- main.go
+++ main.go
@@ -10,21 +10,20 @@ import (
 
 func main() {
 	ExecutionTimerStart := time.Now()
-
 	BlockFile := OpenBlockfile()
 	BlockList := ReturnBlockListFromFile(BlockFile)
-	for i := 0; i < len(BlockList); i++ {
-		fmt.Printf("####### BLOCK INDEX %d ######\n\n\n", i)
-		fmt.Printf("%d", len(BlockList[i]))
-		fmt.Printf("\n\n\n####### END BLOCK INDEX %d ######\n\n\n", i)
+	for i := 0; i < len(*BlockList); i++ {
+		fmt.Printf("####### BLOCK INDEX %d ######\n", i)
+		fmt.Printf("%d", len((*BlockList)[i]))
+		fmt.Printf("\n####### END BLOCK INDEX %d ######\n", i)
 	}
-
 	ExecutionTimeElapsed := time.Since(ExecutionTimerStart)
 	fmt.Printf("+++++EXECUTION TIME TOTAL %s+++++", ExecutionTimeElapsed)
 }
 
-func OpenBlockfile() (BlockFile []byte) {
-	BlockFile, err := os.ReadFile("/Users/jeonkangmin/Desktop/blk00000.dat") // Upload whole block file to memory for faster indexing
+func OpenBlockfile() (BlockFile *[]byte) {
+	blockFile, err := os.ReadFile("/Users/jeonkangmin/Desktop/blk00000.dat") // Upload whole block file to memory for faster indexing
+	BlockFile = &blockFile
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,13 +31,14 @@ func OpenBlockfile() (BlockFile []byte) {
 }
 
 // Types used : []byte, uint32, uint64, bool, int32
-func ReturnBlockListFromFile(BlockFile []byte) [][]byte {
+func ReturnBlockListFromFile(BlockFile *[]byte) (BlockList *[][]byte) {
 	var Position uint32 = 0
-	var BlockFileLength = uint32(len(BlockFile))
-	var BlockList [][]byte
+	var BlockFileLength = uint32(len(*BlockFile))
+	var blockList [][]byte
+	BlockList = &blockList
 	for Position < BlockFileLength {
-		var BlockLength = binary.LittleEndian.Uint32(BlockFile[Position+4 : Position+8])
-		BlockList = append(BlockList, BlockFile[Position:Position+BlockLength])
+		var BlockLength = binary.LittleEndian.Uint32((*BlockFile)[Position+4 : Position+8])
+		blockList = append(blockList, (*BlockFile)[Position:Position+BlockLength])
 		Position = Position + BlockLength + 8
 
 	}
